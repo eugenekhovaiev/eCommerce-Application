c131211254cad728033e1e2a601a12e38cf9e64b
Alena636 <alenahomich636@gmail.com>
2023-08-10 15:26:28 +0300

    feat: add typography

--- src/widgets/AuthForm/LoginForm/login-form.tsx
+++ src/widgets/AuthForm/LoginForm/login-form.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 export const LoginForm: React.FC = () => {
     return (
         <div className="login-form">
-            
+           <Typography variant="h3" gutterBottom className='login-form__title'>
+                Login
+            </Typography> 
         </div>
     )
 }
