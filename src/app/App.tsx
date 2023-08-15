c00594689eedc47134f318d05c1db0b17e82eb3d
Alena636 <alenahomich636@gmail.com>
2023-08-15 14:46:34 +0300

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
 import Login from '../pages/Login/Login';
-import './styles/App.css';
 import Main from '../pages/main/Main';
 import Header from '../widgets/header/Header';
+import { Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+// import Registration;
+// import Page404;
 
 function App(): JSX.Element {
   return (
@@ -9,6 +12,12 @@ function App(): JSX.Element {
       <Header />
       <Login />
       <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path='/registration' element={<Registration />} />
+        <Route path='*' element={<Page404 />} /> */}
+      </Routes>
     </>
   );
 }
