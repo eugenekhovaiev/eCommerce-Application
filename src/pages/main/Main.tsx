e62c8daa85f5b7a613014e501bddf5e7daa62664
XeniyaMV <mehnuchka.xd@gmail.com>
2023-08-14 17:04:28 -0700

    refactor: implement BEM to Main

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,15 +2,15 @@ import './Main.scss';
 
 function Main(): JSX.Element {
   return (
-    <div className="main">
-      <main className="main_content">
-        <div className="start-screen-container">
-          <div className="start-screen">
-            <h2 className="start-screen_title">eCommerce-Application</h2>
+    <main className="main">
+      <section className="start-screen">
+        <div className="container">
+          <div className="start-screen__wrapper">
+            <h2 className="start-screen__title">eCommerce-Application</h2>
           </div>
         </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
 
