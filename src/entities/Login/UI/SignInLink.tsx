14b7de4605bf29fb28f847dac727f5cc91bde54d
Alena636 <alenahomich636@gmail.com>
2023-08-16 10:43:13 +0300

    feat: add link to the login page on header

--- src/entities/Login/UI/SignInLink.tsx
+++ src/entities/Login/UI/SignInLink.tsx
@@ -2,7 +2,7 @@ import Link from '../../../shared/UI/link/Link';
 import { ISignInProps } from '../types';
 
 function SignInLink(props: ISignInProps): JSX.Element {
-  return <Link className={props.className} title="Sign In" onClick={props.onClick} />;
+  return <Link className={props.className} title="Sign In" onClick={props.onClick} href="/login" />;
 }
 
 export default SignInLink;
