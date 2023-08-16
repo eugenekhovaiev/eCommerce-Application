a3edea92e1661f79638fda70f8567f4e9655434a
Alena636 <alenahomich636@gmail.com>
2023-08-16 10:54:23 +0300

    reafctor: change the structure of link

--- src/shared/UI/link/Link.tsx
+++ src/shared/UI/link/Link.tsx
@@ -1,14 +1,15 @@
 import './Link.scss';
 import { ILinkProps } from '../../types';
 import getClassName from '../../lib/helpers/getClassName';
+import { Link } from 'react-router-dom';
 
-function Link(props: ILinkProps): JSX.Element {
+function LinkElement(props: ILinkProps): JSX.Element {
   const className = getClassName('link', props.className);
   return (
-    <a className={className} onClick={props.onClick} href={props.href} target={props.target}>
+    <Link className={className} onClick={props.onClick} to={props.to} target={props.target}>
       {props.title} {props.children}
-    </a>
+    </Link>
   );
 }
 
-export default Link;
+export default LinkElement;
