358d3a1bd8f3ee940ce6d3b1b93301d48f778454
Alena636 <alenahomich636@gmail.com>
2023-08-16 15:10:34 +0300

    feat: add style for body

--- src/pages/404/page-404.tsx
+++ src/pages/404/page-404.tsx
@@ -1,12 +1,10 @@
 import '../404/page-404.css';
 const Page404 = (): JSX.Element => {
   return (
-    // add logo or button
     <div className="page404-container">
       <h2 className="page404-title">404</h2>
       <p className="page404-subtitle">Page Not Found</p>
       <p className="page404-describe">The page you're looking for does not seem to exist</p>
-      {/* add img */}
     </div>
   );
 };
