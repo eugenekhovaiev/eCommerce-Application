09396af61deafa1ae1a18d5069a16d29606c457a
Alena636 <alenahomich636@gmail.com>
2023-08-16 11:03:32 +0300

    feat: add link on logo to the main page

--- src/shared/UI/logo/Logo.tsx
+++ src/shared/UI/logo/Logo.tsx
@@ -1,15 +1,18 @@
 import './Logo.scss';
 import { ILogoProps } from '../../types';
 import getClassName from '../../lib/helpers/getClassName';
+import LinkElement from '../link/Link';
 
 function Logo(props: ILogoProps): JSX.Element {
   const className = getClassName('logo', props.className);
   if (props.iconSrc) {
     return (
-      <h1 className={className} onClick={props.onClick}>
-        <span className="logo__title">{props.title}</span>
-        <img className="logo__icon" src={props.iconSrc} alt="logo"></img>
-      </h1>
+      <LinkElement className={props.className} to="/">
+        <h1 className={className} onClick={props.onClick}>
+          <span className="logo__title">{props.title}</span>
+          <img className="logo__icon" src={props.iconSrc} alt="logo"></img>
+        </h1>
+      </LinkElement>
     );
   }
   return (
