fb28652195e14cfdf5fafb18be9b519e44c6ccaa
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-16 15:53:35 +0300

    feat: add normalize.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
-import Page404 from '../pages/404/page-404';
+import 'normalize.css';
 import './styles/reset.css';
 import './styles/App.css';
+
+import Page404 from '../pages/404/page-404';
 
 function App(): JSX.Element {
   return (
