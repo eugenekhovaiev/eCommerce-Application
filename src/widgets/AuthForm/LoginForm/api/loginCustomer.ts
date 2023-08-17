1511b3dbad777c3b204e7765f85aea3850369407
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-17 17:30:34 +0300

    fix: remove .me() from loginCustomer

--- src/widgets/AuthForm/LoginForm/api/loginCustomer.ts
+++ src/widgets/AuthForm/LoginForm/api/loginCustomer.ts
@@ -4,7 +4,7 @@ import { apiRoot } from '../../../../shared/api/user/UserClient';
 import { ClientResponse, CustomerSignInResult } from '@commercetools/platform-sdk';
 
 const loginCustomer = async (loginData: CustomerSignin): Promise<ClientResponse<CustomerSignInResult>> => {
-  return apiRoot.me().login().post({ body: loginData }).execute();
+  return apiRoot.login().post({ body: loginData }).execute();
 };
 
 export default loginCustomer;
