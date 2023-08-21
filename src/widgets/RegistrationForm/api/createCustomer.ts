213238330dc918addf30a000e33049c19b193ae8
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-21 16:39:40 +0300

    refactor: remove eslint exception from createCustomer

--- src/widgets/RegistrationForm/api/createCustomer.ts
+++ src/widgets/RegistrationForm/api/createCustomer.ts
@@ -1,16 +1,9 @@
-import { CustomerDraft } from '@commercetools/platform-sdk';
+import { CustomerDraft, CustomerSignInResult } from '@commercetools/platform-sdk';
+import { ClientResponse } from '@commercetools/platform-sdk';
+
 import { apiRoot } from '../../../shared/api/user/UserClient';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const createCustomer = (newCustomerData: CustomerDraft) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const testCustomerDraft: CustomerDraft = {
-    email: 'test@test.com',
-    password: 'testPassword1-',
-    firstName: 'TestFirstName',
-    lastName: 'TestLastName',
-  };
-
+const createCustomer = (newCustomerData: CustomerDraft): Promise<ClientResponse<CustomerSignInResult>> => {
   return apiRoot
     .customers()
     .post({
