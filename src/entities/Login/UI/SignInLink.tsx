ed3dc14af847375c0d7677d059ab14ea156cc4ba
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-22 12:59:37 +0300

    refactor: change sign in button to log in

--- src/entities/Login/UI/SignInLink.tsx
+++ src/entities/Login/UI/SignInLink.tsx
@@ -3,9 +3,9 @@ import { ILinkProps } from '../types';
 
 function SignInLink(props: ILinkProps): JSX.Element {
   if (localStorage.getItem('isAuth') === 'true') {
-    return <LinkElement className={props.className} title="Sign In" onClick={props.onClick} to="/" />;
+    return <LinkElement className={props.className} title="Log In" onClick={props.onClick} to="/" />;
   }
-  return <LinkElement className={props.className} title="Sign In" onClick={props.onClick} to="/login" />;
+  return <LinkElement className={props.className} title="Log In" onClick={props.onClick} to="/login" />;
 }
 
 export default SignInLink;
