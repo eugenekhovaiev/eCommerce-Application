a11ed0acb0478f548110cfb7cb409e1638c02cf7
XeniyaMV <mehnuchka.xd@gmail.com>
2023-08-22 17:27:02 -0700

    feat: add building real time validation

--- src/shared/UI/Inputs/address/BuildingShippingInput.tsx
+++ src/shared/UI/Inputs/address/BuildingShippingInput.tsx
@@ -2,8 +2,20 @@ import { TextField } from '@mui/material';
 import { IInputProps } from '../../../types';
 import { Controller } from 'react-hook-form';
 import buildingValidation from '../../../lib/validation/buildingValidation';
+import { useState } from 'react';
+import validateRealTime from '../../../lib/validation/validateRealTime';
 
 const BuildingShippingInput = (props: IInputProps): JSX.Element => {
+  const [isValid, setIsValid] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const value = e.target.value;
+    const isValidValue = validateRealTime(value, buildingValidation.validate).isValid;
+    const messageValue = validateRealTime(value, buildingValidation.validate).message;
+    setIsValid(isValidValue);
+    setMessage(messageValue);
+  };
   return (
     <Controller
       control={props.control}
@@ -16,10 +28,13 @@ const BuildingShippingInput = (props: IInputProps): JSX.Element => {
           color="secondary"
           className={props.className}
           variant={props.variant || 'standard'}
-          onChange={(e): void => field.onChange(e)}
+          onChange={(e): void => {
+            field.onChange(e);
+            handleValueChange(e);
+          }}
           value={field.value || ''}
-          error={!!props.errors.buildingShipping?.message}
-          helperText={props.errors.buildingShipping?.message}
+          error={!!props.errors.buildingShipping?.message || !isValid}
+          helperText={!isValid ? message : props.errors.buildingShipping?.message}
         />
       )}
     />
