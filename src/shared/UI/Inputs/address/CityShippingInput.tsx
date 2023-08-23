9909b3095366502624bb9681042e85118793d889
XeniyaMV <mehnuchka.xd@gmail.com>
2023-08-22 17:33:26 -0700

    feat: add city real time validation

--- src/shared/UI/Inputs/address/CityShippingInput.tsx
+++ src/shared/UI/Inputs/address/CityShippingInput.tsx
@@ -2,8 +2,21 @@ import { TextField } from '@mui/material';
 import { IInputProps } from '../../../types';
 import { Controller } from 'react-hook-form';
 import cityValidation from '../../../lib/validation/cityValidation';
+import { useState } from 'react';
+import validateRealTime from '../../../lib/validation/validateRealTime';
 
 const CityShippingInput = (props: IInputProps): JSX.Element => {
+  const [isValid, setIsValid] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const value = e.target.value;
+    const isValidValue = validateRealTime(value, cityValidation.validate).isValid;
+    const messageValue = validateRealTime(value, cityValidation.validate).message;
+    setIsValid(isValidValue);
+    setMessage(messageValue);
+  };
+
   return (
     <Controller
       control={props.control}
@@ -16,10 +29,13 @@ const CityShippingInput = (props: IInputProps): JSX.Element => {
           color="secondary"
           className={props.className}
           variant={props.variant || 'standard'}
-          onChange={(e): void => field.onChange(e)}
+          onChange={(e): void => {
+            field.onChange(e);
+            handleValueChange(e);
+          }}
           value={field.value || ''}
-          error={!!props.errors.cityShipping?.message}
-          helperText={props.errors.cityShipping?.message}
+          error={!!props.errors.cityShipping?.message || !isValid}
+          helperText={!isValid ? message : props.errors.cityShipping?.message}
         />
       )}
     />
