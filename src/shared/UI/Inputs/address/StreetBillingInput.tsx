0f5b5bb1c179afffed6f9e9c238bf5184420294f
XeniyaMV <mehnuchka.xd@gmail.com>
2023-08-22 18:01:04 -0700

    feat: add street real time validation

--- src/shared/UI/Inputs/address/StreetBillingInput.tsx
+++ src/shared/UI/Inputs/address/StreetBillingInput.tsx
@@ -2,8 +2,20 @@ import { TextField } from '@mui/material';
 import { IInputProps } from '../../../types';
 import { Controller } from 'react-hook-form';
 import streetValidation from '../../../lib/validation/streetValidation';
+import { useState } from 'react';
+import validateRealTime from '../../../lib/validation/validateRealTime';
 
 const StreetBillingInput = (props: IInputProps): JSX.Element => {
+  const [isValid, setIsValid] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const value = e.target.value;
+    const isValidValue = validateRealTime(value, streetValidation.validate).isValid;
+    const messageValue = validateRealTime(value, streetValidation.validate).message;
+    setIsValid(isValidValue);
+    setMessage(messageValue);
+  };
   return (
     <Controller
       control={props.control}
@@ -16,10 +28,13 @@ const StreetBillingInput = (props: IInputProps): JSX.Element => {
           color="secondary"
           className={props.className}
           variant={props.variant || 'standard'}
-          onChange={(e): void => field.onChange(e)}
+          onChange={(e): void => {
+            field.onChange(e);
+            handleValueChange(e);
+          }}
           value={field.value || ''}
-          error={!!props.errors.streetBilling?.message}
-          helperText={props.errors.streetBilling?.message}
+          error={!!props.errors.streetBilling?.message || !isValid}
+          helperText={!isValid ? message : props.errors.streetBilling?.message}
         />
       )}
     />
--- src/shared/UI/Inputs/address/StreetShippingInput.tsx
+++ src/shared/UI/Inputs/address/StreetShippingInput.tsx
@@ -2,8 +2,21 @@ import { TextField } from '@mui/material';
 import { IInputProps } from '../../../types';
 import { Controller } from 'react-hook-form';
 import streetValidation from '../../../lib/validation/streetValidation';
+import { useState } from 'react';
+import validateRealTime from '../../../lib/validation/validateRealTime';
 
 const StreetShippingInput = (props: IInputProps): JSX.Element => {
+  const [isValid, setIsValid] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const value = e.target.value;
+    const isValidValue = validateRealTime(value, streetValidation.validate).isValid;
+    const messageValue = validateRealTime(value, streetValidation.validate).message;
+    setIsValid(isValidValue);
+    setMessage(messageValue);
+  };
+
   return (
     <Controller
       control={props.control}
@@ -16,10 +29,13 @@ const StreetShippingInput = (props: IInputProps): JSX.Element => {
           color="secondary"
           className={props.className}
           variant={props.variant || 'standard'}
-          onChange={(e): void => field.onChange(e)}
+          onChange={(e): void => {
+            field.onChange(e);
+            handleValueChange(e);
+          }}
           value={field.value || ''}
-          error={!!props.errors.streetShipping?.message}
-          helperText={props.errors.streetShipping?.message}
+          error={!!props.errors.streetShipping?.message || !isValid}
+          helperText={!isValid ? message : props.errors.streetShipping?.message}
         />
       )}
     />
