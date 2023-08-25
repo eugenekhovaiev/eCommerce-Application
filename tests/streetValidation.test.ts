e74025b1e2d9e3c6afe0ecf9bf9cac1a9497b105
Alena636 <alenahomich636@gmail.com>
2023-08-25 14:49:32 +0300

    feat: add test for street validation

---
+++ tests/streetValidation.test.ts
@@ -0,0 +1,21 @@
+import streetValidation from '../src/shared/lib/validation/streetValidation';
+
+describe('streetValidation', () => {
+  it('should return true for a valid street address', () => {
+    const validStreet = '123 Main Street';
+    const result = streetValidation.validate(validStreet);
+    expect(result).toBe(true);
+  });
+
+  it('should return "Required" for an empty street address', () => {
+    const emptyStreet = '';
+    const result = streetValidation.validate(emptyStreet);
+    expect(result).toBe('Street address must contain at least one character');
+  });
+
+  it('should return "Street address must contain at least one character" for a street address with only spaces', () => {
+    const spacesStreet = '    ';
+    const result = streetValidation.validate(spacesStreet);
+    expect(result).toBe('Street address must contain at least one character');
+  });
+});
