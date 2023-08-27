755ef1325fa8c21206363bb17c264ffdd803a1b0
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-28 00:40:59 +0300

    refactor: move reouting to separate hook

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,13 @@
-import Login from '../pages/Login/Login';
-import Registration from '../pages/Registration/Registration';
-import Main from '../pages/Main/Main';
 import Header from '../widgets/Header/Header';
-import Page404 from '../pages/Page404/Page404';
-import Cart from '../pages/Cart/Cart';
-import Catalog from '../pages/Catalog/Catalog';
-import Profile from '../pages/Profile/Profile';
-import About from '../pages/About/About';
-
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import { useLoggedInContext } from '../shared/lib/contexts/LoggedInContext';
+import useRoutes from './routes/routes';
 
 function App(): JSX.Element {
-  const { isLoggedIn } = useLoggedInContext();
+  const routes = useRoutes();
 
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
-        <Route path="*" element={<Page404 />} />
-        <Route path="/registration" element={isLoggedIn ? <Navigate to="/" replace /> : <Registration />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      {routes}
     </>
   );
 }
