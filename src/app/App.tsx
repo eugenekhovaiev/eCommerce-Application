748e2ef9b233d6170b292eafbffde365939f6ccd
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-28 02:10:30 +0300

    fix: move routing back to App.tsx trying to pass netlify checks in PR

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,34 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from '../widgets/Header/Header';
-import useRoutes from './routes/routes';
+import Main from '../pages/Main/Main';
+import { Login } from '@mui/icons-material';
+import Page404 from '../pages/Page404/Page404';
+import Registration from '../pages/Registration/Registration';
+import Catalog from '../pages/Catalog/Catalog';
+import Profile from '../pages/Profile/Profile';
+import Cart from '../pages/Cart/Cart';
+import About from '../pages/About/About';
+import { useLoggedInContext } from '../shared/lib/contexts/LoggedInContext';
+// import useRoutes from './routes/routes';
 
 function App(): JSX.Element {
-  const routes = useRoutes();
+  // const routes = useRoutes();
+  const { isLoggedIn } = useLoggedInContext();
 
   return (
     <>
       <Header />
-      {routes}
+      {/* {routes} */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+        <Route path="*" element={<Page404 />} />
+        <Route path="/registration" element={isLoggedIn ? <Navigate to="/" replace /> : <Registration />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
