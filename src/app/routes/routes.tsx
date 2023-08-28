a22d519d7c18a5567e83ea459f3249e716ab3d51
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-08-28 03:08:31 +0300

    fix: remove useRoutes again (WTF I'M DYING)

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,31 +1,31 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+// import { Route, Routes, Navigate } from 'react-router-dom';
 
-import Main from '../../pages/Main/Main';
-import Login from '../../pages/Login/Login';
-import Page404 from '../../pages/Page404/Page404';
-import Registration from '../../pages/Registration/Registration';
-import Catalog from '../../pages/Catalog/Catalog';
-import Profile from '../../pages/Profile/Profile';
-import Cart from '../../pages/Cart/Cart';
-import About from '../../pages/About/About';
+// import Main from '../../pages/Main/Main';
+// import Login from '../../pages/Login/Login';
+// import Page404 from '../../pages/Page404/Page404';
+// import Registration from '../../pages/Registration/Registration';
+// import Catalog from '../../pages/Catalog/Catalog';
+// import Profile from '../../pages/Profile/Profile';
+// import Cart from '../../pages/Cart/Cart';
+// import About from '../../pages/About/About';
 
-import { useLoggedInContext } from '../../shared/lib/contexts/LoggedInContext';
+// import { useLoggedInContext } from '../../shared/lib/contexts/LoggedInContext';
 
-export const useRoutes = (): JSX.Element => {
-  const { isLoggedIn } = useLoggedInContext();
+// export const useRoutes = (): JSX.Element => {
+//   const { isLoggedIn } = useLoggedInContext();
 
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
-      <Route path="*" element={<Page404 />} />
-      <Route path="/registration" element={isLoggedIn ? <Navigate to="/" replace /> : <Registration />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  );
-};
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Main />} />
+//       <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+//       <Route path="*" element={<Page404 />} />
+//       <Route path="/registration" element={isLoggedIn ? <Navigate to="/" replace /> : <Registration />} />
+//       <Route path="/catalog" element={<Catalog />} />
+//       <Route path="/profile" element={<Profile />} />
+//       <Route path="/cart" element={<Cart />} />
+//       <Route path="/about" element={<About />} />
+//     </Routes>
+//   );
+// };
 
-export default useRoutes;
+// export default useRoutes;
