8f274f6305de39f88c679fd8cf5980a6db0770da
Alena636 <alenahomich636@gmail.com>
2023-08-29 16:32:00 +0300

    refactor: change the structure of information about user

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,77 +1,59 @@
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit';
+import { Card, CardContent, Typography, Button } from '@mui/material';
 
 const Profile = (): JSX.Element => {
   return (
     <section className="profile">
       <div className="container profile__container">
         <h2 className="profile__title">Information</h2>
-        <MDBContainer className="py-5">
-          <MDBRow>
-            <MDBCol lg="4">
-              <MDBCard className="mb-4">
-                <MDBCardBody className="text-center">
-                  <MDBCardImage
-                    src="src/shared/assets/user-icon.svg"
-                    alt="avatar"
-                    className="rounded-circle"
-                    style={{ width: '150px' }}
-                    fluid
-                  />
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol lg="8">
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Full Name</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">Johnatan Smith</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Date of birth</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">12-12-2008</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Email</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">example@example.com</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Shipping address</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">Bay Area, San Francisco, CA</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                  <hr />
-                  <MDBRow>
-                    <MDBCol sm="3">
-                      <MDBCardText>Billing address</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                      <MDBCardText className="text-muted">NY</MDBCardText>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
+        <Card>
+          <CardContent>
+            <div className="profile__row">
+              <Typography variant="h6" gutterBottom>
+                Full name
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                First name Last name
+              </Typography>
+            </div>
+            <hr />
+            <div className="profile__row">
+              <Typography variant="h6" gutterBottom>
+                Date of birth
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                12.12.2008
+              </Typography>
+            </div>
+            <hr />
+            <div className="profile__row">
+              <Typography variant="h6" gutterBottom>
+                Email
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                user@user.com
+              </Typography>
+            </div>
+            <hr />
+            <div className="profile__row">
+              <Typography variant="h6" gutterBottom>
+                Shipping Address
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                USA
+              </Typography>
+            </div>
+            <hr />
+            <div className="profile__row">
+              <Typography variant="h6" gutterBottom>
+                Billing Address
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Poland
+              </Typography>
+            </div>
+            <Button variant="contained">Edit Profile</Button>
+          </CardContent>
+        </Card>
       </div>
     </section>
   );
