405a18e739c93da6b371c68bbc0d9d9929e60130
Alena636 <alenahomich636@gmail.com>
2023-09-02 16:46:24 +0300

    feat: add product price, description

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -5,11 +5,26 @@ const Product = (): JSX.Element => {
   return (
     <section className="product">
       <div className="container product__wrapper">
-        <Typography variant="h3" gutterBottom className="product__title">
+        <Typography variant="h3" gutterBottom className="product__page-title">
           Product
         </Typography>
         <SwiperElement />
-        
+        <div className="product__description">
+        <Typography variant="h6" gutterBottom className="product__title">
+          Name of product
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom className="product__price-promotion">
+          promotion price
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom className="product__price-regular">
+          Regular price:
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+          rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+          quasi quidem quibusdam.
+        </Typography>
+        </div>
       </div>
     </section>
   );
