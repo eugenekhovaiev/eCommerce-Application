cfa9cc9d1ac07950995b366c8928a60d1262e75e
Alena636 <alenahomich636@gmail.com>
2023-09-02 13:52:17 +0300

    feat: setup swiperjs

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,5 +1,15 @@
+import { Typography } from '@mui/material';
+
 const Product = (): JSX.Element => {
-  return <section className="product" />;
+  return (
+    <section className="product">
+      <div className="container product__wrapper">
+        <Typography variant="h3" gutterBottom className="product__title">
+          Product
+        </Typography>
+      </div>
+    </section>
+  );
 };
 
 export default Product;
