0c8f0891c7771248fd5d834db07771c310811015
XeniyaMV <mehnuchka.xd@gmail.com>
2023-09-16 19:03:44 -0700

    feat: add PriceElement to cart page

--- src/entities/basket/BasketItem.tsx
+++ src/entities/basket/BasketItem.tsx
@@ -3,10 +3,15 @@ import ButtonElement from '../../shared/UI/buttonElement/ButtonElement';
 import { BasketItemProps } from '../../shared/types';
 import removeProductFromCart from '../../shared/api/user/cart/removeProductFromCart';
 import { useActiveCartContext } from '../../shared/lib/contexts/ActiveCartContext';
+import PriceElement from '../../shared/UI/priceElement/PriceElement';
 
 const BasketItem = (props: BasketItemProps): JSX.Element => {
   const quantity = props.lineItem.quantity;
-  const price = props.lineItem.price.discounted?.value.centAmount || props.lineItem.price.value.centAmount;
+  const priceOriginal = props.lineItem.price.value.centAmount;
+  const priceDiscounted =
+    props.lineItem.discountedPricePerQuantity.length !== 0
+      ? props.lineItem.discountedPricePerQuantity[0].discountedPrice.value.centAmount
+      : props.lineItem.price.discounted?.value.centAmount;
   const total = props.lineItem.totalPrice.centAmount;
   const image =
     props.lineItem.variant.images && props.lineItem.variant.images.length !== 0
@@ -24,16 +29,26 @@ const BasketItem = (props: BasketItemProps): JSX.Element => {
   return (
     <div className="cart-items__item">
       <div className="cart-items__image">
-        <img src={image} alt={'example'} />
+        <img className="image" src={image} alt={name} />
       </div>
       <div className="cart-items__name">{name}</div>
-      <div className="cart-items__price">{`$${(+price / 100).toFixed(2)}`}</div>
+      <div className="cart-items__price">
+        <PriceElement
+          additionalClassName={props.lineItem.discountedPricePerQuantity.length !== 0 ? 'price_discounted' : ''}
+          priceOriginal={priceOriginal}
+          priceDiscounted={priceDiscounted}
+        />
+      </div>
       <div className="cart-items__quantity">
         <SpinnerInput id={id} value={quantity} />
       </div>
       <div className="cart-items__total">
         <span className="title">Total: </span>
-        {`$${(+total / 100).toFixed(2)}`}
+        <PriceElement
+          additionalClassName={props.lineItem.discountedPricePerQuantity.length !== 0 ? 'price_discounted' : ''}
+          priceOriginal={priceOriginal * quantity}
+          priceDiscounted={total}
+        />
       </div>
       <div className="cart-items__close">
         <ButtonElement title="+" onClick={(): Promise<void> => handleOnClick(id)} />
