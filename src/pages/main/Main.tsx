2ae056e5db0c82b24fcb00f4a78a651aaa5112dd
XeniyaMV <mehnuchka.xd@gmail.com>
2023-09-19 00:16:49 -0700

    feat: add coupons to main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -8,8 +8,35 @@ function Main(): JSX.Element {
         <div className="container">
           <div className="start-screen__wrapper">
             <h2 className="start-screen__title">eCommerce-Application</h2>
+            <p>
+              Уважаемый проверяющий, просим Вас подождать с проверкой до 20-го числа. Заранее большое спасибо за
+              понимание))
+            </p>
             <LogInLink additionalClassName="start-screen__link" />
             <SignUpLink additionalClassName="start-screen__link" />
+          </div>
+        </div>
+      </section>
+      <section className="coupons">
+        <div className="container coupons__wrapper">
+          <h2 className="coupons__title">Coupons</h2>
+          <div className="coupons__cards">
+            <div className="coupons__card coupons__card_purple">
+              <div className="coupons__image coupons__image_purple">50%</div>
+              <div className="coupons__code coupons__code_purple">50offcollars</div>
+              <div className="coupons__description coupons__description_purple">Get 50% off on collars</div>
+              <div className="coupons__exceptions coupons__exceptions_purple">
+                The coupon cannot be applied to products on sale.
+              </div>
+            </div>
+            <div className="coupons__card coupons__card_orange">
+              <div className="coupons__image coupons__image_orange">25%</div>
+              <div className="coupons__code coupons__code_orange">25off</div>
+              <div className="coupons__description coupons__description_orange">Get 25% off on all products</div>
+              <div className="coupons__exceptions coupons__exceptions_orange">
+                The coupon cannot be applied to products on sale.
+              </div>
+            </div>
           </div>
         </div>
       </section>
